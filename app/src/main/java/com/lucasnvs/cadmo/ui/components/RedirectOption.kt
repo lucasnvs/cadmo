3a2fdc8147387f3346e4dcca15f1af78b5be1f08
lucasnvs <lucasli.nev@hotmail.com>
2024-01-02 00:13:56 -0300

    fix: DarkGrayColor in RedirectOption

--- app/src/main/java/com/lucasnvs/cadmo/ui/components/RedirectOption.kt
+++ app/src/main/java/com/lucasnvs/cadmo/ui/components/RedirectOption.kt
@@ -3,13 +3,18 @@ package com.lucasnvs.cadmo.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +25,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lucasnvs.cadmo.ui.theme.CadmoTheme
+import com.lucasnvs.cadmo.ui.theme.DarkGrayColor
 
 @Composable
 fun RedirectOption(
@@ -37,8 +43,11 @@ fun RedirectOption(
             .clickable { onClick() }
             .padding(horizontal = 10.dp, vertical = 15.dp)
     ) {
-        Row() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             labelIcon()
+            Spacer(modifier = Modifier.width(5.dp))
             label()
         }
         Icon(imageVector = Icons.Filled.KeyboardArrowRight, contentDescription = "Abrir")
@@ -49,7 +58,15 @@ fun RedirectOption(
 @Composable
 fun RedirectOptionPreview() {
     CadmoTheme {
-        RedirectOption(label = { Text(text = "Hardware") })
+        Column {
+            RedirectOption(label = { Text(text = "Configurações", color = DarkGrayColor)}, labelIcon = { Icon(
+                imageVector = Icons.Filled.Settings,
+                contentDescription = "Configurações",
+                tint = DarkGrayColor
+            )})
+            Spacer(modifier = Modifier.height(2.dp))
+            RedirectOption(label = { Text(text = "Hardware", color = DarkGrayColor) })
+        }
     }
 }
 
@@ -60,10 +77,10 @@ fun ListRedirectOption(items: List<Pair<String, ImageVector?>>) {
     ) {
         items(items) { (title, icon) ->
             RedirectOption(
-                label = { Text(text = title) },
+                label = { Text(text = title, color = DarkGrayColor) },
                 labelIcon = {
                     if(icon != null) {
-                        Icon(imageVector = icon, contentDescription = "Icone de $title")
+                        Icon(imageVector = icon, contentDescription = "Icone de $title", tint = DarkGrayColor)
                     }
                 }
             )
