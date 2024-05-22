1980e014afec28bda07b50fcf06379e4648a90b7
lucasnvs <lucasli.nev@hotmail.com>
2024-05-22 17:35:31 -0300

    Fix: FavoriteScreen Layout

--- app/src/main/java/com/lucasnvs/cadmo/ui/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/lucasnvs/cadmo/ui/favorite/FavoriteScreen.kt
@@ -3,6 +3,7 @@ package com.lucasnvs.cadmo.ui.favorite
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,16 +21,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.lucasnvs.cadmo.R
+import com.lucasnvs.cadmo.domain.model.Product
 import com.lucasnvs.cadmo.ui.CadmoAppState
 import com.lucasnvs.cadmo.ui.components.Product
 import com.lucasnvs.cadmo.ui.components.bars.MainTopBar
 import com.lucasnvs.cadmo.ui.components.bars.NavigationBottomBar
 import com.lucasnvs.cadmo.ui.home.Loading
+import com.lucasnvs.cadmo.ui.shared.ProductItemState
 import com.lucasnvs.cadmo.ui.shared.Screen
 import com.lucasnvs.cadmo.ui.shared.toModel
 import com.lucasnvs.cadmo.ui.theme.CadmoTheme
@@ -64,34 +70,59 @@ fun FavoriteScreen(
             )
         }
     ) { innerPadding ->
-        if(uiState.isLoading) {
-            Loading(modifier = Modifier, innerPadding = innerPadding)
-        } else {
-            Box(
-                modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
-                    .background(Color(0xFFF9F9F9))
-            ) {
+        when {
+            uiState.isLoading -> Loading(modifier = Modifier, innerPadding = innerPadding)
+            uiState.favoriteProducts.isEmpty() -> EmptyContent(modifier = Modifier.padding(innerPadding))
+            else ->
+                Content(
+                    favoriteItems = uiState.favoriteProducts,
+                    onFavoriteClick = viewModel::onFavoriteClick,
+                    modifier = Modifier.padding(innerPadding)
+                )
+        }
+    }
+}
 
-                LazyColumn(
-                    modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.spacedBy(
-                        space = 10.dp,
-                        alignment = Alignment.CenterVertically
-                    ),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    items(uiState.favoriteProducts) {  product ->
-                        Product(
-                            modifier = Modifier,
-                            product = product,
-                            onFavoriteButtonClick = { viewModel.onFavoriteClick(product.toModel(), !product.isFavorite.value)} // provavelmente eu posso mudar todo o valor de ProductUI ou nao, porque nao e necessario o valor mutavel, ao modificar ele modifica o item e a lista por si só
-                        )
-                    }
-                }
+@Composable
+fun Content(
+    favoriteItems: List<ProductItemState>,
+    onFavoriteClick: (Product, Boolean) -> Unit,
+    modifier: Modifier
+) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(Color(0xFFF9F9F9))
+    ) {
+        LazyColumn(
+            modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(
+                space = 10.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(favoriteItems) {  product ->
+                Product(
+                    modifier = Modifier,
+                    product = product,
+                    onBuyButtonClick = {},
+                    onFavoriteButtonClick = { onFavoriteClick(product.toModel(), !product.isFavorite.value)} // provavelmente eu posso mudar todo o valor de ProductUI ou nao, porque nao e necessario o valor mutavel, ao modificar ele modifica o item e a lista por si só
+                )
             }
         }
+    }
+}
+
+
+@Composable
+fun EmptyContent(modifier: Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFF9F9F9))
+    ) {
+        Text(text = stringResource(id = R.string.empty_favorites_list), textAlign = TextAlign.Center)
     }
 }
 
@@ -101,4 +132,16 @@ fun FavoriteScreenPreview() {
     CadmoTheme {
         FavoriteScreen(appState = CadmoAppState(rememberNavController()))
     }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun ContentPreview() {
+//    Content()
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun EmptyContentPreview() {
+    EmptyContent(Modifier)
 }
