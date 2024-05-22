47cf15965410aa0ed210b306c370cde42c179a44
lucasnvs <lucasli.nev@hotmail.com>
2024-05-22 16:34:02 -0300

    Add: Utils

---
+++ app/src/main/java/com/lucasnvs/cadmo/ui/util/CoroutinesUtils.kt
@@ -0,0 +1,7 @@
+package com.lucasnvs.cadmo.ui.util
+
+import kotlinx.coroutines.flow.SharingStarted
+
+private const val StopTimeoutMillis: Long = 5000
+
+val WhileUiSubscribed: SharingStarted = SharingStarted.WhileSubscribed(StopTimeoutMillis)
