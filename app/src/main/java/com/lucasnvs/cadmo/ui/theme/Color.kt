a1ead379e598ca30d2599e29782a85cc33579e3a
lucasnvs <lucasli.nev@hotmail.com>
2024-05-22 22:27:55 -0300

    Fix: Add Alpha

--- app/src/main/java/com/lucasnvs/cadmo/ui/theme/Color.kt
+++ app/src/main/java/com/lucasnvs/cadmo/ui/theme/Color.kt
@@ -11,12 +11,12 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val PrincipalColor = Color(0xFF7B08B1)
-val MediumPrincipalColor = Color(0x49236B)
-val StrongPrincipalColor = Color(0x2C1C33)
+val MediumPrincipalColor = Color(0xFF49236B)
+val StrongPrincipalColor = Color(0xFF2C1C33)
 val LightPrincipalColor = Color(0xFFAC62DA)
 
-val SecondaryColor = Color(0x09B060)
-val StrongSecondaryColor = Color(0x1C3328)
+val SecondaryColor = Color(0xFF09B060)
+val StrongSecondaryColor = Color(0xFF1C3328)
 
 val LightGrayColor = Color(0xFFC3C1C1)
 val DarkGrayColor = Color(0xFF646363)
